e516ebe81d7b9f6df56229bcee1e9ab2b5d65af4
Robert Wood <L.robertwood@gmail.com>
2022-05-13 20:56:18 -0400

    finally pushing this to source control

---
+++ app.js
@@ -0,0 +1,51 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const res = require('express/lib/response');
+
+const app = express();
+
+var cons = require('consolidate');
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// imports for links such as /projects.html and /indexStyle.css
+app.use(express.static('public/images'));
+app.use(express.static('public/pages'));
+app.use(express.static('public/javascripts'));
+app.use(express.static('public/stylesheets'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
